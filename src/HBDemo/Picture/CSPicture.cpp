7e49110d8622cf121188811386f668936d7f432c
黄存璘 <hcl@meitu.com>
2017-08-16 15:34:02 +0800

    [代码提交]  添加图片处理类

---
+++ src/HBDemo/Picture/CSPicture.cpp
@@ -0,0 +1,9 @@
+//
+//  CSPicture.cpp
+//  FFmpeg
+//
+//  Created by zj-db0519 on 2017/8/16.
+//  Copyright © 2017年 meitu. All rights reserved.
+//
+
+#include "CSPicture.hpp"
---
+++ src/HBDemo/Picture/CSPicture.h
@@ -0,0 +1,24 @@
+//
+//  CSPicture.hpp
+//  FFmpeg
+//
+//  Created by zj-db0519 on 2017/8/16.
+//  Copyright © 2017年 meitu. All rights reserved.
+//
+
+#ifndef CSPicture_hpp
+#define CSPicture_hpp
+
+#include <stdio.h>
+/***
+ *  代办事项
+ *  1、转成特定格式裸数据
+ *  2、转成特定编码格式输出
+ *  3、转成特定封装格式输出
+ *  4、图片旋转
+ *  5、图片缩放
+ *  6、图片抠图，截取特定区域图像输出
+ */
+
+
+#endif /* CSPicture_hpp */
