0742a52d177a6dadab3b5ac834bc5bfe8439c335
黄存璘 <hcl@meitu.com>
2017-11-13 23:52:54 +0800

    [代码提交] 新增 csmodule 模块测试代码

--- src/HBDemo/CSModule/CSDemo.cpp
+++ src/HBDemo/CSModule/CSDemo.cpp
@@ -19,126 +19,105 @@
 
 void *audioReadThread(void *arg)
 {
-    int ret;
-    FILE *pcmfp;
-    const char *pcmFile = CS_MODULE_RESOURCE_ROOT_PATH"audio.pcm";
-    bool startFlag = false;
+    int ret = HB_OK;
+    FILE *pPcmFileHandle = nullptr;
+    const char *pInputPcmFile = CS_MODULE_RESOURCE_ROOT_PATH"/100_audio_44100_s16le.pcm";
+    bool bStartFlag = false;
     struct timeval start, end;
-    uint8_t *decodedBuffer;
+    uint8_t *pDecodedBuffer = nullptr;
     int64_t audioTimestamp = 0;
-    HBMedia::CSPlayer::CSPlayer *recordtest;
+    HBMedia::CSPlayer::CSPlayer *pMediaPlayer = (HBMedia::CSPlayer::CSPlayer *)arg;
     
-    recordtest = (HBMedia::CSPlayer::CSPlayer *)arg;
+    pPcmFileHandle = fopen(pInputPcmFile, "r");
+    if (!pPcmFileHandle)
+        LOGE("[Test] ===> Open pcm input file %s error!\n", pInputPcmFile);
+    else
+        bStartFlag = true;
     
-    LOGI("Audio read thread @@@@\n");
-    
-    pcmfp = fopen(pcmFile, "r");
-    if (pcmfp == NULL) {
-        LOGE("Open file %s error!\n", pcmFile);
-        startFlag = false;
-    } else {
-        startFlag = true;
-    }
-    decodedBuffer = (uint8_t *)malloc(SINGLE_SAMPLES);
-    if (decodedBuffer == NULL) {
-        LOGE("Malloc buffer error!\n");
-        goto TAR_OUT;
+    pDecodedBuffer = (uint8_t *)malloc(SINGLE_SAMPLES);
+    if (!pDecodedBuffer) {
+        LOGE("[Test] ===> Malloc decode buffer failed !");
+        goto AUDIO_READ_THREAD_EXIT_LABEL;
     }
     
-    while (startFlag) {
+    LOGI("[Test] ===> Create audio test thread !");
+    while (bStartFlag) {
         gettimeofday(&start, NULL);
-        ret = fread(decodedBuffer, 1, (size_t)SINGLE_SAMPLES, pcmfp);
+        ret = fread((void *)pDecodedBuffer, SINGLE_SAMPLES, 1, pPcmFileHandle);
         if (ret <= 0) {
-            LOGE("Read pcm file error!\n");
+            LOGE("[Test] ===> Read pcm data from input file error !");
             break;
         }
         
-        ret = recordtest->writeExternData(decodedBuffer, SINGLE_SAMPLES, 1, audioTimestamp);
+        ret = pMediaPlayer->writeExternData(pDecodedBuffer, SINGLE_SAMPLES, 1, audioTimestamp);
         if (ret < 0) {
-            LOGE("Audio Write data error![%d]\n", ret);
+            LOGE("[Test] ===> Audio Write data error![%d]\n", ret);
         }
+        
         usleep(1500);
         gettimeofday(&end, NULL);
-        //        _DEBUG_PRINT("Write audio data use time %ld us\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec));
+        
         audioTimestamp += 15;
     }
+    LOGI("[Test] ===> Audio test thread exit !");
     
-TAR_OUT:
+AUDIO_READ_THREAD_EXIT_LABEL:
+    if (pDecodedBuffer)
+        free(pDecodedBuffer);
     
-    if (decodedBuffer) {
-        free(decodedBuffer);
-    }
-    
-    if (pcmfp) {
-        fclose(pcmfp);
-    }
+    if (pPcmFileHandle)
+        fclose(pPcmFileHandle);
     
     return NULL;
 }
 
 void *videoReadThread(void *arg)
 {
-    int ret;
-    int ySize;
-    int inWidth = 960, inHeight = 540;
-    long bufSize;
-    FILE *yuvfp;
+    int ret = HB_OK;
+    FILE *pInputYuvHandle = nullptr;
+    const char *pInputYuvFile = CS_MODULE_RESOURCE_ROOT_PATH"/100_video_480_480_420p.yuv";
+    long iDecodedBufferLen = av_image_get_buffer_size(AV_PIX_FMT_YUV420P, 480, 480, 0);
     int64_t videoTimestamp = 0;
-    uint8_t *decodedBuffer;
+    uint8_t *pDecodedBuffer = nullptr;
     struct timeval start, end;
-    HBMedia::CSPlayer::CSPlayer *recordtest;
-    const char *yuvfile = CS_MODULE_RESOURCE_ROOT_PATH"yuv420p.yuv";
-    bool startFlag = false;
+    bool bStartFlag = false;
+    HBMedia::CSPlayer::CSPlayer *pMediaPlayer = (HBMedia::CSPlayer::CSPlayer *)arg;
+    pInputYuvHandle = fopen(pInputYuvFile, "r");
+    if (pInputYuvHandle == NULL)
+        LOGE("[Test] ===> Open yuv input file %s error!\n", pInputYuvFile);
+    else
+        bStartFlag = true;
     
-    recordtest = (HBMedia::CSPlayer::CSPlayer *)arg;
-    
-    LOGI("video read thread @@@@\n");
-    
-    yuvfp = fopen(yuvfile, "r");
-    if (yuvfp == NULL) {
-        LOGE("Open file %s error!\n", yuvfile);
-        startFlag = false;
-    } else {
-        startFlag = true;
-    }
-    
-    ySize = inWidth * inHeight;
-    //  huangcl 待定
-//    bufSize = param.readinVideoBufSize();
-    decodedBuffer = (uint8_t *)malloc(bufSize);
-    if (decodedBuffer == NULL) {
+    pDecodedBuffer = (uint8_t *)malloc(iDecodedBufferLen);
+    if (pDecodedBuffer == NULL) {
         LOGE("Malloc decoded data error!\n");
-        goto TAR_OUT;
+        goto VIDEO_READ_THREAD_EXIT_LABEL;
     }
     
-    while (startFlag) {
+    LOGI("[Test] ===> Create Video test thread !");
+    while (bStartFlag) {
         gettimeofday(&start, NULL);
-        ret = fread(decodedBuffer, 1, (size_t)bufSize, yuvfp);
+        ret = fread(pDecodedBuffer, 1, (size_t)iDecodedBufferLen, pInputYuvHandle);
         if (ret <= 0) {
-            LOGE("Read yuv file error!\n");
+            LOGE("[Test] ===> Read yuv file error!\n");
             break;
         }
         usleep(2000);
-        ret = recordtest->writeExternData(decodedBuffer, bufSize, 0, videoTimestamp);
+        ret = pMediaPlayer->writeExternData(pDecodedBuffer, iDecodedBufferLen, 0, videoTimestamp);
         if (ret < 0) {
-            LOGE("Video Write data error![%d]\n", ret);
+            LOGE("[Test] ===> Video Write data error![%d]\n", ret);
         }
         gettimeofday(&end, NULL);
-        //        _DEBUG_PRINT("Write video data use time %ld us\n", 1000000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec));
         videoTimestamp += 33.3;
     }
+    LOGI("[Test] ===> Video test thread exit !");
     
-    LOGW("Video thread exit!\n");
+VIDEO_READ_THREAD_EXIT_LABEL:
+    if (pDecodedBuffer)
+        free(pDecodedBuffer);
     
-TAR_OUT:
-    
-    if (decodedBuffer) {
-        free(decodedBuffer);
-    }
-    
-    if (yuvfp) {
-        fclose(yuvfp);
-    }
+    if (pInputYuvHandle)
+        fclose(pInputYuvHandle);
     
     return NULL;
 }
--- src/HBDemo/CSModule/CSDemo.h
+++ src/HBDemo/CSModule/CSDemo.h
@@ -10,6 +10,11 @@
 #define CSDemo_h
 
 #include <stdio.h>
+/**
+ *  使用ffmpeg获取PCM数据:
+ *      ffmpeg -i input.mpg -f s16le -ar 44100 -acodec pcm_s16le output.pcm
+ */
+
 #define CS_MODULE_RESOURCE_ROOT_PATH "/Users/zj-db0519/work/code/github/HbFFmpegMedia/resource/CSModule"
 
 #endif /* CSDemo_h */
