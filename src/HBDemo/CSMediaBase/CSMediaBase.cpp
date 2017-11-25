baadfe0e2d5faf141a99f3832762cf3ea6f86da8
黄存璘 <hcl@meitu.com>
2017-11-25 09:22:04 +0800

    [代码提交]  新增基类

---
+++ src/HBDemo/CSMediaBase/CSMediaBase.cpp
@@ -0,0 +1,9 @@
+//
+//  CSMediaBase.cpp
+//  FFmpeg
+//
+//  Created by zj-db0519 on 2017/11/25.
+//  Copyright © 2017 meitu. All rights reserved.
+//
+
+#include "CSMediaBase.hpp"
---
+++ src/HBDemo/CSMediaBase/CSMediaBase.h
@@ -0,0 +1,14 @@
+//
+//  CSMediaBase.hpp
+//  FFmpeg
+//
+//  Created by zj-db0519 on 2017/11/25.
+//  Copyright © 2017 meitu. All rights reserved.
+//
+
+#ifndef CSMediaBase_hpp
+#define CSMediaBase_hpp
+
+#include <stdio.h>
+
+#endif /* CSMediaBase_hpp */
