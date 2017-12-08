d251d5ee8cf8874814f0aedb07f4e7dc5041246f
黄存璘 <hcl@meitu.com>
2017-12-08 10:58:13 +0800

    [代码提交] 汇总对应帧信息

--- src/HBDemo/CSModule/CSTools/CSVideo/CSVideoAnalysis.cpp
+++ src/HBDemo/CSModule/CSTools/CSVideo/CSVideoAnalysis.cpp
@@ -10,11 +10,23 @@
 
 namespace HBMedia {
 
+static void initialFrameInfoSummary(CSFrameSummaryInfo* summary) {
+    summary->mAvgInteral = 0;
+    summary->mTotalInteral = 0;
+    summary->mMinInteral = 0;
+    summary->mMaxInteral = 0;
+}
+
 CSVideoAnalysis::CSVideoAnalysis()
 : CSVideoDecoder()
 {
+    mVideoFrameCount = 0;
+    mAudioFrameCount = 0;
     memset(&mInfoSet, 0xFF, sizeof(mInfoSet));
     memset(&mVideoPictureTypeArry, 0x00, sizeof(int64_t));
+    
+    initialFrameInfoSummary(&mIFrameSummary);
+    initialFrameInfoSummary(&mPFrameSummary);
 }
 
 CSVideoAnalysis::~CSVideoAnalysis() {
@@ -22,27 +34,98 @@ CSVideoAnalysis::~CSVideoAnalysis() {
 }
 
 void CSVideoAnalysis::analysisFrame(AVFrame *pFrame, AVMediaType type) {
-    
-    EchoFrameInfo(pFrame, type);
-
-    if (mInfoSet & INFO_FRAME_TYPE) {
-        /** 统计不同帧类型的数据情况 */
-        mVideoPictureTypeArry[pFrame->pict_type]++;
-    }
+    _collectFrameInfo(pFrame, type);
 }
 
-void CSVideoAnalysis::EchoFrameInfo(AVFrame *pFrame, AVMediaType type) {
-    if (type == AVMEDIA_TYPE_VIDEO) {
+void CSVideoAnalysis::_collectFrameInfo(AVFrame *pFrame, AVMediaType type) {
+    if (type == AVMEDIA_TYPE_VIDEO)
+    {
         IMAGE_PIX_FORMAT eLocalVideoPixFormat = getImageExternFormat((AVPixelFormat)pFrame->format);
+        int64_t tiExternPts = av_rescale_q(pFrame->pts, \
+                                    mPInVideoFormatCtx->streams[mVideoStreamIndex]->time_base, AV_TIME_BASE_Q);
+        double  tfExternPts = ((double)tiExternPts / AV_TIME_BASE);
+        
+        if (mInfoSet & INFO_FRAME_TYPE) {
+            /** 统计不同帧类型的数据情况 */
+            mVideoFrameCount++;
+            mVideoPictureTypeArry[pFrame->pict_type]++;
+            
+            CSFrameInfo newFrameInfoObj;
+            newFrameInfoObj.mFrameIndex = mVideoPictureTypeArry[pFrame->pict_type];
+            newFrameInfoObj.mPTS = tfExternPts;
+            newFrameInfoObj.mFrameOffset = mVideoFrameCount;
+            
+            switch (pFrame->pict_type) {
+                case AV_PICTURE_TYPE_I:
+                    {
+                        {
+                            /** 计算 P 帧时间间隔 */
+                            if (mPFrameSet.size() != 0) {
+                                mPFrameSummary.mTotalInteral += mPFrameSet.size();
+                                
+                                if (mPFrameSummary.mMinInteral == 0)
+                                    mPFrameSummary.mMinInteral = (int)mPFrameSet.size();
+                                else if (mPFrameSummary.mMinInteral > mPFrameSet.size())
+                                    mPFrameSummary.mMinInteral = (int)mPFrameSet.size();
+                                
+                                if (mPFrameSummary.mMaxInteral < mPFrameSet.size())
+                                    mPFrameSummary.mMaxInteral = (int)mPFrameSet.size();
+                            }
+                            mPFrameSet.clear();
+                        }
+                        
+                        {
+                            /** 计算 B 帧时间间隔 */
+                            if (mBFrameSet.size() != 0) {
+                                mBFrameSummary.mTotalInteral += mBFrameSet.size();
+                                
+                                if (mBFrameSummary.mMinInteral == 0)
+                                    mBFrameSummary.mMinInteral = (int)mBFrameSet.size();
+                                else if (mBFrameSummary.mMinInteral > mBFrameSet.size())
+                                    mBFrameSummary.mMinInteral = (int)mBFrameSet.size();
+                                
+                                if (mBFrameSummary.mMaxInteral < mBFrameSet.size())
+                                    mBFrameSummary.mMaxInteral = (int)mBFrameSet.size();
+                            }
+                            mBFrameSet.clear();
+                        }
+                        
+                        /** 计算 I 帧时间间隔 */
+                        if (mIFrameSet.size() != 0) {
+                            size_t iLastFrameInfoIndex = (mIFrameSet.size() - 1);
+                            double currentFrameInteral = newFrameInfoObj.mFrameOffset - mIFrameSet[iLastFrameInfoIndex].mFrameOffset;
+                            
+                            mIFrameSummary.mTotalInteral += currentFrameInteral;
+                            if (currentFrameInteral > mIFrameSummary.mMaxInteral)
+                                mIFrameSummary.mMaxInteral = currentFrameInteral;
+                            
+                            if (mIFrameSummary.mMinInteral == 0)
+                                mIFrameSummary.mMinInteral = currentFrameInteral;
+                            else if (currentFrameInteral < mIFrameSummary.mMinInteral)
+                                mIFrameSummary.mMinInteral = currentFrameInteral;
+                        }
+                        mIFrameSet.push_back(newFrameInfoObj);
+                    }
+                    break;
+                case AV_PICTURE_TYPE_P:
+                    {
+                        mPFrameSet.push_back(newFrameInfoObj);
+                    }
+                    break;
+                case AV_PICTURE_TYPE_B:
+                    {
+                        mBFrameSet.push_back(newFrameInfoObj);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        
         LOGI("[Echo Frame: <%s>]: Size<%d,%d> Format<%s> AspectRatio<%d:%d>", \
              getPictureTypeDescript(pFrame->pict_type), pFrame->width, pFrame->height, \
              getImagePixFmtDescript(eLocalVideoPixFormat), \
              pFrame->sample_aspect_ratio.num, pFrame->sample_aspect_ratio.den);
-        
-        
-        int64_t tiExternPts = av_rescale_q(pFrame->pts, mPInVideoFormatCtx->streams[mVideoStreamIndex]->time_base, \
-                                        AV_TIME_BASE_Q);
-        double  tfExternPts = ((double)tiExternPts / AV_TIME_BASE);
         
         LOGI("         (%lf) ===> PTS<%lld> BestPTS<%lld> DTS<%lld> codecd_no<%d> display_no<%d>", \
              tfExternPts,  pFrame->pts, pFrame->best_effort_timestamp, pFrame->pkt_dts, \
@@ -54,13 +137,18 @@ void CSVideoAnalysis::ExportAnalysisInfo() {
     LOGI(">>> ========================================");
     if (mInfoSet & INFO_FRAME_TYPE) {
         LOGI("Frame Type: I | P | B | S | SI | SP | BI | NONE >>> ");
-        LOGI("  I-frame: <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_I]);
-        LOGI("  P-frame: <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_P]);
-        LOGI("  B-frame: <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_B]);
-        LOGI("  S-frame: <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_S]);
-        LOGI("  SI-frame: <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_SI]);
-        LOGI("  SP-frame: <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_SP]);
-        LOGI("  BI-frame: <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_BI]);
+        LOGI("  I-frame:    <no:%lld> Frame-Interal: <Max:%d> <Min:%d> <Avg:%d>", \
+             mVideoPictureTypeArry[AV_PICTURE_TYPE_I], mIFrameSummary.mMaxInteral, mIFrameSummary.mMinInteral, (int)(mIFrameSummary.mTotalInteral / (int)(mIFrameSet.size() - 1)));
+        
+        LOGI("  P-frame:    <no:%lld> Per_I_Frame: <Max:%d> <Min:%d> <Avg:%d>", \
+             mVideoPictureTypeArry[AV_PICTURE_TYPE_P], mPFrameSummary.mMaxInteral, mPFrameSummary.mMinInteral, (int)(mPFrameSummary.mTotalInteral / (int)(mIFrameSet.size() - 1)));
+        
+        LOGI("  B-frame:    <no:%lld> Per_I_Frame: <Max:%d> <Min:%d> <Avg:%d>", \
+             mVideoPictureTypeArry[AV_PICTURE_TYPE_B], mBFrameSummary.mMaxInteral, mBFrameSummary.mMinInteral, (int)(mBFrameSummary.mTotalInteral / (int)(mIFrameSet.size() - 1)));
+        LOGI("  S-frame:    <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_S]);
+        LOGI("  SI-frame:   <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_SI]);
+        LOGI("  SP-frame:   <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_SP]);
+        LOGI("  BI-frame:   <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_BI]);
         LOGI("  NONE-frame: <no:%lld>", mVideoPictureTypeArry[AV_PICTURE_TYPE_NONE]);
         LOGI("=============================================== <<< ");
     }
--- src/HBDemo/CSModule/CSTools/CSVideo/CSVideoAnalysis.h
+++ src/HBDemo/CSModule/CSTools/CSVideo/CSVideoAnalysis.h
@@ -10,57 +10,82 @@
 #define CSVideoAnalysis_h
 
 #include <stdio.h>
+#include <vector>
 #include "CSVideoDecoder.h"
 
 namespace HBMedia {
 
 /** 根据 AVPictureType 类型的最大个数来定 */
 #define MAX_PICTURE_TYPE  (8)
-    
+
 #define INFO_FRAME_TYPE    (1 << 0)
 #define INFO_FRAME_ENCODER (1 << 1)
 #define INFO_FRAME_DECODER (1 << 2)
 #define INFO_FRAME_OTHER   (1 << 3)
-    
+
+    typedef struct CSFrameInfo {
+        int64_t mFrameIndex;
+        int64_t mFrameOffset;
+        double mPTS;
+    } CSFrameInfo;
+
+    typedef struct CSFrameSummaryInfo {
+        int mMaxInteral;
+        int mMinInteral;
+        int mAvgInteral;
+        int mTotalInteral;
+    } CSFrameSummaryInfo;
+
 /** 结构功能解析:
  *  视频文件解析
  */
-typedef class CSVideoAnalysis : public CSVideoDecoder
-{
-public:
-    CSVideoAnalysis();
-    
-    ~CSVideoAnalysis();
-    
-    /**
-     *  对外接口，分析输入的上一帧图像数据
-     */
-    void analysisFrame(AVFrame *pFrame, AVMediaType type);
-    
-    /**
-     *  输出汇总后视频解析信息
-     */
-    void ExportAnalysisInfo();
-    
-    void setAnalysisModule(uint64_t infoSet) { mInfoSet = infoSet; }
-    
-protected:
-    
-    /**
-     *  内部输出当前帧的一些信息
-     */
-    void EchoFrameInfo(AVFrame *pFrame, AVMediaType type);
-    
-private:
-    uint64_t mInfoSet;
-    
-    /** Video info  */
-    int64_t mVideoPictureTypeArry[MAX_PICTURE_TYPE];
-    
-    
-    /** Audio info  */
-    
-} CSVideoAnalysis;
+    typedef class CSVideoAnalysis : public CSVideoDecoder {
+    public:
+        CSVideoAnalysis();
+
+        ~CSVideoAnalysis();
+
+        /**
+         *  对外接口，分析输入的上一帧图像数据
+         */
+        void analysisFrame(AVFrame *pFrame, AVMediaType type);
+
+        /**
+         *  输出汇总后视频解析信息
+         */
+        void ExportAnalysisInfo();
+
+        void setAnalysisModule(uint64_t infoSet) {
+            mInfoSet = infoSet;
+        }
+
+    protected:
+
+        /**
+         *  内部输出当前帧的一些信息
+         */
+        void _collectFrameInfo(AVFrame *pFrame, AVMediaType type);
+
+    private:
+        uint64_t mInfoSet;
+        
+        /** Video info  */
+        int64_t mVideoPictureTypeArry[MAX_PICTURE_TYPE];
+        int64_t mVideoFrameCount;
+        int64_t mAudioFrameCount;
+        
+        std::vector<CSFrameInfo> mIFrameSet;
+        CSFrameSummaryInfo mIFrameSummary;
+        
+        std::vector<CSFrameInfo> mPFrameSet;
+        CSFrameSummaryInfo mPFrameSummary;
+        
+        std::vector<CSFrameInfo> mBFrameSet;
+        CSFrameSummaryInfo mBFrameSummary;
+
+        /** Audio info  */
+
+    } CSVideoAnalysis;
 
 }
 
