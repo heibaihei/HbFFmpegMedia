d8b8f4859e525f49588b9059db8f2d3e43a9c2f0
黄存璘 <hcl@meitu.com>
2018-01-23 00:52:21 +0800

    [代码提交]  对绘制三角型相关执行流程进行描述

--- proj/FFmpeg/Sample/main.cpp
+++ proj/FFmpeg/Sample/main.cpp
@@ -67,7 +67,7 @@ int main(int argc, const char * argv[]) {
 //            CSGL_Demo_Tutorial01();
 //            CSGL_Demo_Tutorial02();
 //            CSGL_Demo_Tutorial03();
-//            CSGL_Demo_Tutorial04();
+            CSGL_Demo_Tutorial04();
 //            CSGL_Demo_Tutorial05();
 //            CSGL_Demo_Tutorial06();
 //            CSGL_Demo_Tutorial07();
